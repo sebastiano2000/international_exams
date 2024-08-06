b732ea0d02ffad8320f97c15a266de43bd87ea40
sebastiano2000 <sebastianosaad@gmail.com>
2024-08-06 16:47:10 +0300

    Refactor attachment file storage path and video retrieval in SubjectController

--- resources/views/admin/pages/attachments.php
+++ resources/views/admin/pages/attachments.php
@@ -4,34 +4,6 @@
     <div class="page-wrapper">
         <div class="content container-fluid">
             <div class="page-header">
-                <!-- @if(!Auth::user()->suspend)
-                    <div class="row row-cols-2 justify-content-center"> 
-                        <div class="col-auto mt-30">
-                            <a href="{{ route('exam.try', ['subject_id' => $subjects->first()->id]) }}">
-                                <div class="container-tenant mb-4" style=" width: 411px; height: 411px;">
-                                    <img src="{{ asset('admin_assets/images/vocab-demo.jpg') }}" alt="homepage" class="imgage-fluid" style="height: 100%;width:100%; " /> 
-                                    <div class="card-footer bg-primary w-100 text-white ">DEMO</div>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-auto mt-30">
-                            <a href="{{ route('exam.try', ['subject_id' => $subjects->get(1)->id]) }}">
-                                <div class="container-tenant mb-4" style=" width: 411px; height: 411px;">
-                                    <img src="{{ asset('admin_assets/images/grammar-demo.jpg') }}" alt="homepage" class="imgage-fluid" style="height: 100%;width:100%;" /> 
-                                    <div class="card-footer bg-primary w-100 text-white ">DEMO</div>
-                                </div>                            
-                            </a>
-                        </div>
-                        <div class="col-auto mt-30">
-                            <a href="{{ route('exam.try', ['subject_id' => $subjects->last()->id]) }}">
-                                <div class="container-tenant mb-4" style=" width: 411px; height: 411px;">
-                                    <img src="{{ asset('admin_assets/images/reading-demo.jpg') }}" alt="homepage" class="imgage-fluid" style="height: 100%;width:100%;" /> 
-                                    <div class="card-footer bg-primary w-100 text-white ">DEMO </div>
-                                </div>                             
-                            </a>
-                        </div>
-                    </div>
-                @endif -->
                 @if(!Auth::user()->isAdmin())
                     <div class="row justify-content-center">
                         @foreach($attachments as $attachment)
