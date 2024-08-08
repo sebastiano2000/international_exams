e7209f68c286f6abb053d6c0d6757483fc4c4431
sebastiano2000 <sebastianosaad@gmail.com>
2024-08-08 13:19:35 +0300

    Refactor AskController routes in admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AskController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserResultController;
 use App\Http\Controllers\ResultController;
@@ -61,9 +62,9 @@ Route::group(['prefix' => 'preparator'],function(){
 });
 
 Route::group(['prefix' => 'ask'],function(){
-    Route::get('/', [PreparatorController::class,"index"])->name('preparator');
-    Route::get('/filter',[PreparatorController::class,'filter'])->name('preparator.filter');
-    Route::post('/modify',[PreparatorController::class,'modify'])->name('ask.modify');
+    Route::get('/', [AskController::class,"index"])->name('preparator');
+    Route::get('/filter',[AskController::class,'filter'])->name('preparator.filter');
+    Route::post('/modify',[AskController::class,'modify'])->name('ask.modify');
 });
 
 Route::group(['prefix' => 'user_result'],function(){
